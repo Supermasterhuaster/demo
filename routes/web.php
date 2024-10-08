1fc79f3a11a9e44cc9af7cf36105f0a6ff2e4e1d
s.borzykh <s.borzykh@b2b-center.ru>
2024-10-08 14:52:50 +0500

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::post('/create', [UserController::class, 'create']);
+Route::get('/get/{id?}', [UserController::class, 'get']);
+Route::patch('/update/{id}', [UserController::class, 'update']);
+Route::delete('/delete/{id?}', [UserController::class, 'delete']);
